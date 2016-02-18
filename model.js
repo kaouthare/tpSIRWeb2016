b823c476f9a291e67f7d8e22342704b0366746c7
kaouthar <kaou.bennouna@gmail.com>
2016-02-18 21:57:26 +0100

    Model Q6

--- model.js
+++ model.js
@@ -1,3 +1,37 @@
 
 // Impl√©menter ici les 4 classes du mod√®le.
 // N'oubliez pas l'h√©ritage !
+
+//Class Drawing
+function Drawing() {
+    //DÈclarer un array
+    this.forme = new Array();
+};
+
+
+//Classe forme d'ou va hÈriter les formes : rÈctangle et ligne
+function forme(epaisseur, couleur) {
+    this.epaisseur=epaisseur;
+    this.couleur=couleur;
+};
+
+//Classe Rectangle
+function Rectangle(orgX, orgY, larg, haut, epaisseur, couleur) {
+    forme.call(this, epaisseur, couleur);
+    this.orgX=orgX;
+    this.orgY=orgY;
+    this.larg=larg;
+    this.haut=haut;
+};
+Rectangle.prototype = new forme();
+
+
+//Classe Line
+function Line(xA, yA, xB, yB, epaisseur, couleur) {
+    forme.call(this, epaisseur, couleur);
+    this.xA=xA;
+    this.yA=yA;
+    this.xB=xB;
+    this.yB=yB;
+};
+Line.prototype = new forme();
